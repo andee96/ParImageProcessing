7c94126d15b07aa42bbd050584056cc2243cd0d4
thatGreekGuy96 <andreas@continuum.industries>
2018-11-20 10:57:44 +0000

    update files and add cstlib include statement

--- main_reconstruct_image.cpp
+++ main_reconstruct_image.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+#include <string>
 #include "comms_lib.h"
 #include "mpi.h"
 #include "precision.h"
@@ -9,37 +13,68 @@ using namespace std;
 
 RealNumber boundaryval(int i, int m);
 void set_sawtooth_values(int N_local, int M_local, int N, RealNumber** arr, int* coords);
+void read_params(char* filename, char* file_in, char* file_out, double &delta_max, double &pM, double &pN);
+void read_params(char* filename, char* file_in, char* file_out, double &delta_max, double &pM, double &pN, int &ni, int &nj);
 
 int main(int argc, char **argv)
 {
     int rank, size;
     int M, N; 
-    int coords[2] = {0,0};
-    // Define source and destination names 
+    double pM, pN, delta_max; 
     char filename[100]; 
-    sprintf(filename, "%s", argv[1]); 
     char destname[100];
-    double delta_max = atof(argv[3]);
+    int coords[2] = {0,0};
+    int auto_flag = atoi(argv[2]);
+    int ni=0, nj=0;
+    double t1, t2, ttotal = 0;
+    int n_runs;
+    if (argc == 4){n_runs = atoi(argv[3]);}
+    else{n_runs = 1;}    
+
+    // Read parameters according to auto flag
+    if (auto_flag == 0)
+    {
+        read_params(argv[1], filename, destname, delta_max, pM, pN);
+    }
+    else if (auto_flag == 1)
+    {
+        read_params(argv[1], filename, destname, delta_max, pM, pN, ni, nj);
+    }
     int M_local, N_local; 
 
-    // Initialize 
-    initialize(rank, size, coords); 
+    // Initialize according to the value of the flag 
+    if (auto_flag == 0){ initialize(rank, size, coords);}
+    else if (auto_flag == 1){ initialize(rank, size, coords, ni, nj);}
+    int** proc_dims = create2darray<int>(size, 2, 0);
     if (rank == 0 ){cout << "Reconstructig original image from edge file: " << filename << endl;}
-    sprintf(destname, "%s_%d_%2f.pgm", argv[2], size, delta_max);
+    sprintf(destname, "%s_%d_%2f.pgm", destname, size, delta_max);
     // Read the local buffer
-    if (rank == 0 ){cout << "Reading file..." << endl;}
-    RealNumber** edge_local = read(filename, rank, M_local, N_local, M, N); 
-    if (rank == 0 ){cout << "Done." << endl;}
-    RealNumber** old = create2darray<RealNumber>(M_local+2, N_local+2, 255);
-    RealNumber** new_arr = create2darray<RealNumber>(M_local+2, N_local+2, 255);
-    // Set sawtooth values for old 
-    set_sawtooth_values(N_local, M_local, N, old, coords);
-    if (rank == 0 ){cout << "Reconstructing..." << endl;}
-    reconstruct(delta_max, old, new_arr, edge_local, M_local, N_local, rank);
-    if (rank == 0 ){cout << "Writing file to: " << destname << endl;}
-    write(destname, rank, new_arr, M_local, N_local); 
-    if (rank == 0 ){cout << "Done." << endl;}
-    delete[] edge_local;
+    for (int n = 0; n < n_runs; n++)
+    {
+        if (rank == 0 ){cout << "Run " << n << endl;}
+        // add function for timing in comms library
+        RealNumber** edge_local = read(filename, rank, M_local, N_local, M, N, pM, pN, size, proc_dims); 
+        // cout <<"Rank " << rank << ": Local sizes: " << M_local << ", " << N_local << endl;
+        // if (rank == 0 ){cout << "Done." << endl;}
+        RealNumber** old = create2darray<RealNumber>(M_local+2, N_local+2, 255);
+        RealNumber** new_arr = create2darray<RealNumber>(M_local+2, N_local+2, 255);
+        // Set sawtooth values for old 
+        set_sawtooth_values(N_local, M_local, N, old, coords);
+        // if (rank == 0 ){cout << "Reconstructing..." << endl;}
+        t1 = record_time();
+        reconstruct(delta_max, old, new_arr, edge_local, M_local, N_local, rank);
+        t2 = record_time();
+        ttotal = ttotal + t2 - t1; 
+        // if (rank == 0 ){cout << "Writing file to: " << destname << endl;}
+        delete[] edge_local; delete[] old; 
+        write(destname, rank, new_arr, proc_dims, size); 
+    }
+    if (rank == 0 )
+    {
+        cout << "Done." << endl;
+        cout << "Total time spent reconstructing: " << ttotal << "s." << endl;
+        cout <<"Time per reconstruction: " << ttotal/n_runs << "s." << endl; 
+    }
     finalize();
 }
 
@@ -64,3 +99,55 @@ void set_sawtooth_values(int N_local, int M_local, int N, RealNumber** arr, int*
         arr[M_local+1][j] = int(255*val);
     }
 }
+
+void read_params(char* filename, char* file_in, char* file_out, double &delta_max, double &pM, double &pN)
+{
+    // Reads the parameters from the specified file 
+    string line; 
+    ifstream file(filename);
+
+    // Read parameters line by line 
+    getline(file, line);
+    strcpy(file_in, line.c_str());
+    getline(file, line);
+    strcpy(file_out, line.c_str());
+    getline(file, line);
+    delta_max = atof(line.c_str());
+    getline(file, line);
+    pM = atof(line.c_str());
+    getline(file, line);
+    pN = atof(line.c_str());
+
+    file.close();
+}
+
+void read_params(char* filename, char* file_in, char* file_out, double &delta_max, double &pM, double &pN, int &ni, int &nj)
+{
+    // Reads the parameters from the specified file 
+    string line; 
+    ifstream file(filename);
+
+    // Read parameters line by line 
+    getline(file, line);
+    strcpy(file_in, line.c_str());
+    getline(file, line);
+    strcpy(file_out, line.c_str());
+    getline(file, line);
+    delta_max = atof(line.c_str());
+    getline(file, line);
+    pM = atof(line.c_str());
+    getline(file, line);
+    pN = atof(line.c_str());
+    getline(file, line);
+    ni = atoi(line.c_str());
+    getline(file, line);
+    nj = atoi(line.c_str()); 
+
+    file.close();
+}
+
+// void write_log_file(char* filename, char* file_in, char* file_out, double ttotal, int n_runs)
+// {
+//     return 0;
+// }
+
